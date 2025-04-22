019d0f0bade11161818ccb2d38cfba64f7cb4983
CTardencilla <calebtardencilla@gmail.com>
2025-04-22 07:50:01 -0600

    formato space

--- actividad3/ejercicio1.py
+++ actividad3/ejercicio1.py
@@ -5,7 +5,7 @@ calif3= int(input('Ingrese la calificacion 3: '))
 suma= calif1 + calif2 + calif3 
 promedio= suma/3
 
-print(f"""Calificacion 1: {calif1} 
-calificacion 2: {calif2}
-calificacion 3: {calif3}
-Promedio {promedio:.0f}""")
+print(f"""Calificacion 1: {calif1:>3} 
+calificacion 2: {calif2:>3}
+calificacion 3: {calif3:>3}
+Promedio: {promedio:>9.0f}""")
