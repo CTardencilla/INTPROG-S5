fa584f2877e54f2dc3ebe4f631a4af86c69883a5
CTardencilla <calebtardencilla@gmail.com>
2025-04-22 07:31:53 -0600

    calificaciones

---
+++ actividad3/ejercicio1.py
@@ -0,0 +1,11 @@
+calif1= int(input('Ingrese la calificacion 1: '))
+calif2= int(input('Ingrese la calificacion 2: '))
+calif3= int(input('Ingrese la calificacion 3: '))
+
+suma= calif1 + calif2 + calif3 
+promedio= suma/3
+
+print(f"""Calificacion 1: {calif1} 
+calificacion 2: {calif2}
+calificacion 3: {calif3}
+Promedio {promedio:.0f}""")
