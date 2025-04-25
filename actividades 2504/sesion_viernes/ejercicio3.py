0bbfd8c0932ae8e7f1ebbff0a93c18c196cb5af6
CTardencilla <calebtardencilla@gmail.com>
2025-04-25 08:14:42 -0600

    dias

---
+++ actividades 2504/sesion_viernes/ejercicio3.py
@@ -0,0 +1,18 @@
+num= int(input('Ingrese un el numero del 1-7: '))
+
+if num == 1:
+    print('Lunes')
+elif num == 2:
+    print('Martes')
+elif num == 3:
+    print('Miercoles')
+elif num == 4:
+    print('Jueves')
+elif num == 5:
+    print('Viernes')
+elif num == 6:
+    print('Sabado')
+elif num == 7:
+    print('Domingo')
+elif num >7:
+    print('Numero no valido')
